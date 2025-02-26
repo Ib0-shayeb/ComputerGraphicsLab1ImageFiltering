0bf10ac30bbc37fd675642fdffef1f960f2103a7
Ib0-shayeb <ibrahimowich1@gmail.com>
2025-02-26 23:58:28 +0100

    first commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComputerGraphicsLab1_ImageFiltering.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ComputerGraphicsLab1_ImageFiltering.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
